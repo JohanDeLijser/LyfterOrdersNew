f05aec3bf7a9fad2a65490e50b13f82e5a283109
Johan de Lijser <johan@lyfter.nl>
2021-09-12 16:33:33 +0200

    Add settings page

---
+++ LyfterOrders/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LyfterOrders.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+        public SettingsViewModel()
+        {
+            Title = "Settings";
+            SaveSettings = new Command(async () => await Browser.OpenAsync("https://lyfter.nl"));
+        }
+
+        public ICommand SaveSettings { get; }
+    }
+}
